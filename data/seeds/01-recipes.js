38bcf4343e098384f9953b2a7b2c88e6c8830e34
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-11-07 16:54:43 +0100

    migrations and seeds done

---
+++ data/seeds/01-recipes.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("recipes")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("recipes").insert([
+        { recipe_name: "Pizza" },
+        { recipe_name: "Pasta" }
+      ]);
+    });
+};
